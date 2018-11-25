e89d19e2a48b8a5d11d935610910f506f8b54ba2
Elias James Fox <andorien@gmail.com>
2018-11-24 19:59:54 -0500

    Starting point; echo passed arguments

--- trand/main.swift
+++ trand/main.swift
@@ -8,5 +8,8 @@
 
 import Foundation
 
-print("Hello, World!")
-
+if CommandLine.arguments.count > 0 {
+    for i in 1..<CommandLine.arguments.count {
+        print(CommandLine.arguments[i])
+    }
+}
